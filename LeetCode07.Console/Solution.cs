e68eeae72c2f791735d1511a964c8b149c6e8650
givemin5 <givemin5@gmail.com>
2018-02-18 12:57:23 +0800

    LeetCode 07

---
+++ LeetCode07.Console/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode07.Console
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            List<int> tmp = new List<int>();
+            while (x != 0)
+            {
+                var i = x % 10;
+                x = (x - i) / 10;
+                tmp.Add(i);
+            }
+
+            long p = 0;
+            foreach (var item in tmp)
+            {
+                p = p * 10 + item;
+            }
+
+            try
+            {
+                return checked((int)p);
+            }
+            catch (Exception)
+            {
+
+                return 0;
+            }
+        }
+    }
+}
---
+++ LeetCode07.ConsoleTests/SolutionTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode07.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode07.Console.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void Reverse_123_ShouldBe_321()
+        {
+            Solution soluction = new Solution();
+
+            var input = 123;
+
+            var expected = 321;
+
+            var actual = soluction.Reverse(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ReverseTest_n123_ShouldBe_n32145r()
+        {
+            Solution soluction = new Solution();
+
+            var input = -123;
+
+            var expected = -321;
+
+            var actual = soluction.Reverse(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void ReverseTest_1534236469_ShoudBe_zero()
+        {
+            Solution soluction = new Solution();
+
+            var input = 1534236469;
+
+            var expected = 0;
+
+            var actual = soluction.Reverse(input);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
